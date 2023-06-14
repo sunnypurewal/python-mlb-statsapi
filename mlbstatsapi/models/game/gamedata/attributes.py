89411d1c66b1e176750fa89a290cf0adb46896b2
sunny <sunnypurewal@gmail.com>
2023-06-13 20:32:03 -0400

    optionalize gameinfo

--- mlbstatsapi/models/game/gamedata/attributes.py
+++ mlbstatsapi/models/game/gamedata/attributes.py
@@ -165,9 +165,9 @@ class GameInfo:
     delaydurationminutes : int
         The length of delay for the game in minutes
     """
-    attendance: int
-    firstpitch: str
-    gamedurationminutes: int
+    attendance: Optional[int]
+    firstpitch: Optional[str]
+    gamedurationminutes: Optional[int]
     delaydurationminutes: Optional[int] = None
 
 
